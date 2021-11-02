0f8821576e7419206846470b85052848bcf870db
Ilya Alexeenkov <rmfighht@yandex.ru>
2021-11-02 09:22:32 +0300

    Подключил маску для телефона, сдал работу заказчику

--- js/burger&&scroll.js
+++ js/burger&&scroll.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('.burger');
 const burgerIcon = document.querySelector('.burger__icon');
 const menu = document.querySelector('.header__list');
 const menuLinks = document.querySelectorAll('.scroll-to-item');
+const scrollButton = document.querySelectorAll('.option__button');
 
 burger.addEventListener('click', () => {
   burgerIcon.classList.toggle('active');
@@ -10,6 +11,9 @@ burger.addEventListener('click', () => {
 
 for (let link of menuLinks) {
   link.addEventListener('click', (event) => {
+    if (event.target.classList.contains('option__button')) {
+      event.target.closest('.option__card-wrapper').click();
+    }
     event.preventDefault();
     burgerIcon.classList.remove('active');
     menu.classList.remove('active');
